9e47ae1010f6939939c8f8d14263e415b7684255
aokad <aokada@ims.u-tokyo.ac.jp>
2016-09-06 11:41:09 +0900

    modify fusionfusion

--- genomon_post_analysis/sample_conf.py
+++ genomon_post_analysis/sample_conf.py
@@ -14,8 +14,9 @@ class Sample_conf(object):
         self.sv_detection = []
         self.qc = []
         self.control_panel = {}
-        self.fusionfusion = []
+        self.fusion = []
         self.expression = []
+        self.intron_retention = []
         # 
         # should add the file exist check here ?
         #
@@ -111,6 +112,7 @@ class Sample_conf(object):
         qc_sampleID_list = []
         ff_sampleID_list = []
         exp_sampleID_list = []
+        ir_sampleID_list = []
         
         for row in _data:
             if row[0].startswith('['):
@@ -140,15 +142,18 @@ class Sample_conf(object):
                 elif row[0].lower() == '[controlpanel]':
                     mode = 'controlpanel'
                     continue
-                elif row[0].lower() == '[fusionfusion]':
-                    mode = 'fusionfusion'
+                elif row[0].lower() == '[fusion]':
+                    mode = 'fusion'
                     continue
                 elif row[0].lower() == '[expression]':
                     mode = 'expression'
                     continue
+                elif row[0].lower() == '[intron_retention]':
+                    mode = 'intron_retention'
+                    continue
                 else:
                     err_msg = "Section name should be either of [fastq], [bam_tofastq], [bam_import], " + \
-                              "[mutation_call], [sv_detection], [controlpanel], [fusionfusion] or [expression]. " + \
+                              "[mutation_call], [sv_detection], [controlpanel], [fusion], [expression] or [intron_retention]. " + \
                               "Also, sample name should not start with '['."
                     raise ValueError(err_msg)
             
@@ -318,22 +323,22 @@ class Sample_conf(object):
                 self.control_panel[controlpanelID] = row[1:]
 
 
-            elif mode == 'fusionfusion':
+            elif mode == 'fusion':
 
                 sampleID = row[0]
                 if sampleID not in sampleID_list:
-                    err_msg = "[fusionfusion] section, " + sampleID + " is not defined"
+                    err_msg = "[fusion] section, " + sampleID + " is not defined"
                     raise ValueError(err_msg)
 
                 if sampleID in ff_sampleID_list:
-                    err_msg = "[fusionfusion] section, " + sampleID + " is duplicated"
+                    err_msg = "[fusion] section, " + sampleID + " is duplicated"
                     raise ValueError(err_msg)
 
                 controlpanelID = row[1] if len(row) >= 2 and row[1] not in ['', 'None'] else None
 
                 ff_sampleID_list.append(sampleID)
 
-                self.fusionfusion.append((sampleID,controlpanelID))
+                self.fusion.append((sampleID,controlpanelID))
 
 
             elif mode == 'expression':
@@ -351,6 +356,21 @@ class Sample_conf(object):
 
                 self.expression.append(sampleID)
 
+
+            elif mode == 'intron_retention':
+
+                sampleID = row[0]
+                if sampleID not in sampleID_list:
+                    err_msg = "[intron_retention] section, " + sampleID + " is not defined"
+                    raise ValueError(err_msg)
+
+                if sampleID in ir_sampleID_list:
+                    err_msg = "[intron_retention] section, " + sampleID + " is duplicated"
+                    raise ValueError(err_msg)
+
+                ir_sampleID_list.append(sampleID)
+
+                self.intron_retention.append(sampleID)
 
         # check whether controlpanleID in compare section is defined
         # for comp in self.compare:
